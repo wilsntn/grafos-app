26957dad93dbbd03505c6e87087cb8fefb82db62
wilsntn <wilcson.denner@gmail.com>
2023-05-28 09:56:10 -0400

    feat: removed graph generation and added read_edge list function

--- backend/src/graphs/service.py
+++ backend/src/graphs/service.py
@@ -1,17 +1,7 @@
+import os
 import networkx as nx
-import json
 
-def convert_file_to_json(file):
-    content = file.read().decode('utf-8-sig')
-    graph = nx.Graph()
-
-    for line in content.splitlines():
-        nodes = line.split()
-        if len(nodes) == 2:
-            node1, node2 = nodes
-            graph.add_edge(int(node1), int(node2))
-
+def convert_file_to_json(file_path):
+    graph = nx.read_edgelist(file_path, create_using=nx.Graph(), nodetype = int)
     graph_json = nx.node_link_data(graph)
-    graph_json_str = json.dumps(graph_json)
-
-    return graph_json_str
+    return graph_json
