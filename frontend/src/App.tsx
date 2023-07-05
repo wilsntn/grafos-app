115815cf8b7ccf4fd53da5fb1cfc01d467d868a7
wilsntn <wilcson.denner@gmail.com>
2023-07-04 21:11:06 -0400

    refactor: moved fron next to react-ts

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl h-screen font-bold bg-orange-600 underline">
+      Hello Baga!
+    </h1>
+    </>
+  )
+}
+
+export default App
